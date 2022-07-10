9209dd26977b8cb3b2ef5a40429f3f765d9d062e
SindiswaT <sindiswatoli@gmail.com>
2022-07-10 14:46:52 +0200

    added account file

--- E-commerce/main.js
+++ E-commerce/main.js
@@ -11,6 +11,8 @@ function menutoggle(){
     }
 };
 
+
+
 /********* PRODUCT GALLERY **********/
 
 var ProductImg = document.getElementById("ProductImg");
@@ -35,3 +37,25 @@ SmallImg[3].onclick = function(){
 SmallImg[4].onclick = function(){
     ProductImg.src = SmallImg[4].src;
 };
+
+
+
+
+/********* TOGGLE FORM **********/
+
+var LoginForm = document.getElementById("LoginForm");
+var RegForm = document.getElementById("RegForm");
+var Indicator = document.getElementById("Indicator");
+
+    function register(){
+        RegForm.style.transform = "translateX(0px)";
+        LoginForm.style.transform = "translateX(0px)";
+        Indicator.style.transform = "translateX(100px)";
+    }
+
+    function login(){
+        RegForm.style.transform = "translateX(300px)";
+        LoginForm.style.transform = "translateX(300px)";
+        Indicator.style.transform = "translateX(0px)";
+
+    }
