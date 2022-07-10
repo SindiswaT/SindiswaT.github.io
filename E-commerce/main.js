b9bcd582668772593acc50515203baa45f6eae3a
SindiswaT <sindiswatoli@gmail.com>
2022-07-10 14:56:25 +0200

    connect pages

--- E-commerce/main.js
+++ E-commerce/main.js
@@ -38,24 +38,3 @@ SmallImg[4].onclick = function(){
     ProductImg.src = SmallImg[4].src;
 };
 
-
-
-
-/********* TOGGLE FORM **********/
-
-var LoginForm = document.getElementById("LoginForm");
-var RegForm = document.getElementById("RegForm");
-var Indicator = document.getElementById("Indicator");
-
-    function register(){
-        RegForm.style.transform = "translateX(0px)";
-        LoginForm.style.transform = "translateX(0px)";
-        Indicator.style.transform = "translateX(100px)";
-    }
-
-    function login(){
-        RegForm.style.transform = "translateX(300px)";
-        LoginForm.style.transform = "translateX(300px)";
-        Indicator.style.transform = "translateX(0px)";
-
-    }
